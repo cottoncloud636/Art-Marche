4eb896934ddfce627a5c3e44cf967790d5fa086d
Natasha Zhou <35412065+cottoncloud636@users.noreply.github.com>
2023-12-19 22:15:13 -0800

    created frontend pages and setup routes

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom'
+import Home from './frontend-pages/Home';
+import Register from './frontend-pages/Register';
+import Login from './frontend-pages/Login';
+import About from './frontend-pages/About';
+import Detail from './frontend-pages/Detail';
+
 
 export default function App() {
-  return (
-    <div className='text-green-600'>App</div>
-  )
+  return (<BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/detail' element={<Detail />} />
+
+
+    </Routes>
+    </BrowserRouter>
+    
+  );
 }
