e03a7461a5592c2686c353929db5bd42a78306cb
Natasha Zhou <35412065+cottoncloud636@users.noreply.github.com>
2023-12-21 15:32:54 -0800

    create schema and user model for DB

---
+++ server-side/models/users.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema( {
+    userName: {
+        type: String,
+        unique : true,
+        required: true, 
+    },
+    email: {
+        type: String,
+        unique : true,
+        required: true, 
+    },
+    password: {
+        type: String,
+        unique : true,
+        // required: true -- no need this, because multiple users can have multiple same psw they want to
+    }
+}, 
+//besides that, timestamps info is very useful, will record time of user account created and updated, which is
+//useful for searching user account in the future
+{
+    timestamps : true
+}
+);
+const User = mongoose.model('User', userSchema);
+export default User;
